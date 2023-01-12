41c9df3036414951bdd479f7b15a42b3ebb65d13
Andrey <averel7log@gmail.com>
2023-01-12 12:55:28 +0300

    init commit

---
+++ src/main/java/com/mahov/homeManegment/HomeManegmentApplication.java
@@ -0,0 +1,13 @@
+package com.mahov.homeManegment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeManegmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeManegmentApplication.class, args);
+	}
+
+}
